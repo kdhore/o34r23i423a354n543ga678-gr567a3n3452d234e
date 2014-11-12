5c9a93e162fa2efaa191d0869c651ccaa87c4155
kdhore <karthik.dhore@gmail.com>
2014-11-11 19:34:17 -0500

    Matlab files

---
+++ Decisions.m
@@ -0,0 +1,17 @@
+classdef Decisions
+    % This is the decision class.  The input to this class is all the
+    % YearData objects as well as the OJGame object.  It then sets the
+    % properties values of this class to the decisions. This can obviously
+    % call other functions or have other inputs from other functions as
+    % we deem fit. It can also take as input any sort of metrics and such
+    % that we have calculated from any other function or class.
+    
+    properties
+        % The decisions
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ Simulation.m
@@ -0,0 +1,14 @@
+classdef Simulation
+    % This class runs iterations of the game.
+    %   Instanciated based on whatever yearly data you want to choose
+    %   as well as the OJGame state, and the decisions you want to test
+    %   during the simulation.  
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
