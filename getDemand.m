26e3c206e4f3bf8f449b6a490a2cae22b256f30a
mich1092 <mscharfs@princeton.edu>
2014-11-19 23:05:39 -0500

    new function

---
+++ getDemand.m
@@ -0,0 +1,14 @@
+function [demand] = getDemand(product_index,region_index,price)
+    if (product_index == 1)
+        coeff_matrix = ORA_fits(region_index,:);
+    elseif (product_index == 2)
+        coeff_matrix = POJ_fits(region_index,:);
+    elseif (product_index == 3)
+        coeff_matrix = ROJ_fits(region_index,:);
+    elseif (product_index == 4)
+        coeff_matrix = FCOJ_fits(region_index,:);
+    else
+        display('gave wrong product index');
+    end
+    demand = coeff_matrix.*[1;price;price^2];
+end
