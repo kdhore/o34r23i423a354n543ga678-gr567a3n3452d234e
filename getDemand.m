1de252aac644f9eb26bb9fba19431304de0fd3e2
kdhore <karthik.dhore@gmail.com>
2014-11-19 20:55:01 -0500

    demand and regions

--- getDemand.m
+++ getDemand.m
@@ -249,22 +249,5 @@ for i = 1:7 %regions
      plot(compiled_prices_ROJ, compiled_sales_ROJ, 'o', x2, y2.*x2);
      title(['ROJ, Region ', region{i}]);
 
-function [ ORA_demand, POJ_demand, FCOJ_demand, ROJ_demand, transport_cost ] = getDemand(cities)
-    % Will return the demand for each city
-    ORA_demand = 0;
-    POJ_demand = 0;
-    FCOJ_demand = 0;
-    ROJ_demand = 0;
-    for i = 1:numel(cities/3)
-       ora = oraDemand(cities{i,1});
-       poj = pojDemand(cities{i,1});
-       fcoj = fcojDemand(cities{i,1});
-       roj = rojDemand(cities{i,1});
-       transport_cost = 1.2*cities{i,3}*(ora+poj+fcoj+roj);
-       ORA_demand = ORA_demand + ora;
-       POJ_demand = POJ_demand + poj;
-       FCOJ_demand = FCOJ_demand + fcoj;
-       ROJ_demand = ROJ_demand + roj;
-    end
-end
+
 
--- getRegion.m
+++ getRegion.m
@@ -1,14 +1,32 @@
-function [ region ] = getRegion( city )
+function [ region, region_ind ] = getRegion( city )
 %Returns region of city
-if sum(strcomp(city,{'ANY';'BOS';'CLP';'KEE';'LAK';'MBK';'MVY';'PGH';'PHI';'PVD';'RER';'SCR';'SMS';'SUP'})) == 1
-    region = 1;
+if sum(strcmp(city,{'ANY';'BOS';'CLP';'KEE';'LAK';'MBK';'MVY';'PGH';'PHI';'PVD';'RER';'SCR';'SMS';'SUP'})) == 1
+    region = 'NE';
+    region_ind = 1;
 end
-if sum(strcomp(city,{'CRS';'CVE';'CWV';'DTN';'FRY';'HIP';'JTC';'LXK';'MAO';'MAY';'MSD';'MSP';'MTH';'RCH';'RRN';'SHK';'TIL'})) == 1
-    region = 2;
+if sum(strcmp(city,{'CRS';'CVE';'CWV';'DTN';'FRY';'HIP';'JTC';'LXK';'MAO';'MAY';'MSD';'MSP';'MTH';'RCH';'RRN';'SHK';'TIL'})) == 1
+    region = 'MA';
+    region_ind = 2;
 end
-if sum(strcomp(city,{'CRS';'CVE';'CWV';'DTN';'FRY';'HIP';'JTC';'LXK';'MAO';'MAY';'MSD';'MSP';'MTH';'RCH';'RRN';'SHK';'TIL'})) == 1
-    region = 2;
+if sum(strcmp(city,{'CHR';'DAY';'FPR';'FSC';'GRN';'HVA';'JFL';'MTG';'OCL';'PAN';'WPB';'YEM'})) == 1
+    region = 'SE';
+    region_ind = 3;
 end
-
+if sum(strcmp(city,{'ABL';'BYO';'CED';'CUP';'ELK';'FWA';'GBW';'GEE';'GFK';'HER';'JAC';'LSL';'MAS';'MND';'NLW';'OWT';'SDL';'SHL';'SJF';'STP';'SWI';'TRV'})) == 1
+    region = 'MW';
+    region_ind = 4;
+end
+if sum(strcmp(city,{'BST';'DEL';'ELP';'FTW';'GRL';'LAF';'LRO';'MCX';'MKO';'MRE';'PRA';'RSW';'SGE';'SME';'SNA';'TYE'})) == 1
+    region = 'DS';
+    region_ind = 5;
+end
+if sum(strcmp(city,{'BTT';'DIM';'EUG';'LEW';'PCO';'RSP';'TWF';'YKM'})) == 1
+    region = 'NW';
+    region_ind = 6;
+end
+if sum(strcmp(city,{'BKR';'DOZ';'FSO';'GRU';'HUL';'LOS';'RFE';'RNE';'SCM';'SFC';'TCY'})) == 1
+    region = 'SW';
+    region_ind = 7;
+end
 end
 
