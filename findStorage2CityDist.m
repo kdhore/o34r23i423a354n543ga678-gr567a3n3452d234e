656fc3d9e4e946c13be97a6d5cfd9c23475b5b1c
mich1092 <mscharfs@princeton.edu>
2014-11-21 13:31:13 -0500

    x

--- findStorage2CityDist.m
+++ findStorage2CityDist.m
@@ -1,10 +1,6 @@
-function [dist] = findStorage2CityDist(storage,city)
+function [dist] = findStorage2CityDist(storage,city,storage_units,cities,storage2market_dist)
     j = -1;
     k = -1;
-    [~,~, storage2market_dist] = xlsread('StaticData','S2M','C2:BU101');
-    storage2market_dist = cell2mat(cellNaNReplace(storage2market_dist,0)); 
-    [~,~, cities] = xlsread('StaticData','S2M','B2:B101');
-    [~,~, storage_units] = xlsread('StaticData','P2S','A2:A72');
     for i = 1:length(storage_units)
         if strcmp(storage_units(i), storage)
             j = i;   
