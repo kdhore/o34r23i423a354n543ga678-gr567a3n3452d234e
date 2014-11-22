b21a60508585074e605ec9685ce7ede5e832f73b
mich1092 <mscharfs@princeton.edu>
2014-11-22 12:37:46 -0500

    updated to not use find distances things

--- matchRegiontoStorage.m
+++ matchRegiontoStorage.m
@@ -1,7 +1,23 @@
-function [storage] = matchRegiontoStorage(region,OJgameobj)
+function [storage] = matchRegiontoStorage(region,OJgameobj,distances)
     storage_open = find(OJgameobj.storage_cap);
-    cities = matchRegiontoCities(region);
-    storageFacilities = matchStoragetoCity(cities, storage_open);
+    if strcmp(region, 'NE')
+        cities = [1:14];
+    elseif strcmp(region, 'MA')
+        cities = [15:31];
+    elseif strcmp(region, 'SE')
+        cities = [32:43];
+    elseif strcmp(region, 'MW')
+        cities = [44:65];
+    elseif strcmp(region, 'DS')
+        cities = [66:81];
+    elseif strcmp(region, 'NW')
+        cities = [82:89];
+    elseif strcmp(region, 'SW')
+        cities = [90:100];
+    else
+        display('region not inputted correctly');
+    end
+    storageFacilities = matchStoragetoCity(cities, storage_open, distances);
     [storages, ~, indices] = unique(storageFacilities);
     numOfEachStorage = accumarray(indices(:),1);
     [~, index] = max(numOfEachStorage);
--- matchStoragetoCity.m
+++ matchStoragetoCity.m
@@ -1,9 +1,10 @@
-function [output] = matchStoragetoCity(cities, storage_open)
+function [output] = matchStoragetoCity(cities, storage_open, distances)
 output = cell(length(cities),1);
 for i = 1:length(cities)
     short_dist = 9999999999;
     for j = 1:length(storage_open)
-        distance = findStorage2CityDist(char(storageNamesInUse(storage_open(j))),cities{i});
+        distance = distances(cities(i),storage_open(j));
+        %distance = findStorage2CityDist(char(storageNamesInUse(storage_open(j))),cities{i});
         if (distance < short_dist)
             short_dist = distance;
             closest_storage = char(storageNamesInUse(storage_open(j)));
