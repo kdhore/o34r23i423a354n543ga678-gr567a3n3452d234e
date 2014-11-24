084dfe9ce3ccda8acc7efa48a795ef654e54ca3d
PhilChang <prchang@princeton.edu>
2014-11-23 23:28:36 -0500

    some more edits

---
+++ DecisionsPrices.m
@@ -0,0 +1,13 @@
+%price script
+pricesORA = zeros(7,12);
+pricesPOJ = zeros(7,12);
+pricesROJ = zeros(7,12);
+pricesFCOJ = zeros(7,12);
+for i = 1:7
+    for j = 1:12
+        pricesORA(i,j) = 3.5;
+        pricesPOJ(i,j) = 3.5;
+        pricesROJ(i,j) = 3.5;
+        pricesFCOJ(i,j) = 3.5;
+    end
+end
