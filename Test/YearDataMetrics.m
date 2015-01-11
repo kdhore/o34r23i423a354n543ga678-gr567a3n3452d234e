7af9c22ea37b4c1bf7a5f86a16c1d8dd70c8587a
PhilChang <prchang@princeton.edu>
2015-01-11 18:08:57 -0500

    metrics facility proposals
    run the script

--- Test/YearDataMetrics.m
+++ Test/YearDataMetrics.m
@@ -1,19 +1,20 @@
 function [] = YearDataMetrics(YearData, ojObject)
 
-%create relevant oj object
-initial_str = 'Excel Files Orianga/oriangagrande2014Results.xlsm';
-yearFiles = dir('YearData Orianga/*.mat');
-ojObject = OJGame(initial_str);
-for i = 1:(length(yearFiles))
-    year = load(strcat('YearData Orianga/',yearFiles(i).name));
-    yearVar = genvarname(strrep(yearFiles(i).name,'.mat',''));
-    ojObject = ojObject.update(year.(yearVar));
-end
+
+% %create relevant oj object
+% initial_str = 'Excel Files Orianga/oriangagrande2014Results.xlsm';
+% yearFiles = dir('YearData Orianga/*.mat');
+% ojObject = OJGame(initial_str);
+% for i = 1:(length(yearFiles))
+%     year = load(strcat('YearData Orianga/',yearFiles(i).name));
+%     yearVar = genvarname(strrep(yearFiles(i).name,'.mat',''));
+%     ojObject = ojObject.update(year.(yearVar));
+% end
 
 %find storages open
 stor_open = find(ojObject.storage_cap);
 
-prompt = strcat('Report for year', YearData.year, '\n',...
+prompt = strcat('Report for year', num2str(YearData.year), '\n',...
     'What graph do you want to see? Options are: \n',...
     'a Sales v Availability for each storage','\n',...
     'b Sales by product for each month over a region','\n',...
@@ -21,13 +22,24 @@ prompt = strcat('Report for year', YearData.year, '\n',...
     'd All','\n');
 option = input(prompt);
 
+
 if strcmp(option,'a') || strcmp(option,'d')
     %for each storage unit, plot available vs. sold for each product
     weeks = zeros(1,48);
     for i = 1:48
         weeks(1,i) = i;
     end
+    
+    capacity_adj = input('Enter parameter n of scaling capacity = n * mean \n');
+    
+    
     for i = 1:length(stor_open)
+        
+        total_cap = zeros(1,48);
+        for j = 1:48
+            total_cap(1,j) = ojObject.storage_cap(stor_open(i));
+        end
+        
         figure();
         subplot(2,2,1);
         plot(weeks,...
@@ -84,6 +96,38 @@ if strcmp(option,'a') || strcmp(option,'d')
         ylabel('FCOJ Amount');
         legend('Available', 'Sold');
         
+        total_available = (YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).ORA_Sales(1,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).POJ_Sales(1,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).ROJ_Sales(1,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).FCOJ_Sales(1,:));
+        total_sold = (YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).ORA_Sales(2,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).POJ_Sales(2,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).ROJ_Sales(2,:)+...
+            YearData.storage_res(char...
+            (storageNamesInUse(stor_open(i)))).FCOJ_Sales(2,:));
+        
+        proposed_cap = zeros(1,48);
+        for k = 1:48
+            proposed_cap(1,k) = (mean(total_available)+mean(total_sold))/2*capacity_adj;
+        end
+        
+        figure(); %plot total available, total sold, capacity, proposed capacity
+        plot(weeks,total_available, weeks, total_sold,...
+            weeks,total_cap, weeks, proposed_cap);
+        legend('Total Available', 'Total Sold', strcat(num2str(YearData.year),'Capacity'),...
+            strcat(num2str(YearData.year),'Proposed Decision =', ...
+            num2str(proposed_cap(1,1)-total_cap(1,1)),...
+            'using parameter of ', num2str(capacity_adj)));
+        ylim([0,ojObject.storage_cap(stor_open(i)) + 1000]);
+        xlabel('Weeks');
+        ylabel('Product (Tons)');
     end
 end
 
